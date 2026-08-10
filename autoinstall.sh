d8fb7a7613a54355cf49fe332e3062519472a05e
ArtikciNarek2006 <84734263+ArtikciNarek2006@users.noreply.github.com>
2026-08-10 06:55:44 +0400

    fix aghues url. fix instalsh run with root

--- autoinstall.sh
+++ autoinstall.sh
@@ -46,7 +46,7 @@ echo -e "${GRE}Installed dependencies successfully.${RES}\n"
 echo -e "${CYAN}Downloading aghues... ${RES}"
 TMP_DIR=$(mktemp -d)
 
-if ! wget 'https://ggg.nairi.education/db/get?id=1ed8dc57-f49c-4b5e-acbe-7196c98f421f-93cb734c-09b2-42bb-99c7-b68cefd5bd71' -O "${TMP_DIR}/aghues.gz"; then
+if ! wget 'https://github.com/ArtikciNarek2006/aghues-installation/releases/download/aghues_1.4.4/aghues.gz' -O "${TMP_DIR}/aghues.gz"; then
   echo -e "${RED}ERROR: Downloading failed. Please check connection.${RES}"
   rm -rf "${TMP_DIR}"
   exit 1
@@ -65,16 +65,31 @@ fi
 rm -rf "${TMP_DIR}"
 echo -e "${GRE}Extracted successfully.${RES}"
 
+
 echo "Build/Install ..."
 if [ -d "${AGH_ROOT}/aghues" ]; then
-  cd "${AGH_ROOT}/aghues"
-  if ! CFLAGS="-O2 -Wno-error=int-conversion -Wno-error=implicit-function-declaration -fpermissive" ./install.sh; then
+  cd "${AGH_ROOT}/aghues" || exit 1
+
+  if sudo -n true 2>/dev/null || [ -t 0 ]; then
+    # Terminal or cached sudo session
+    sudo CFLAGS="-O2 -Wno-error=int-conversion -Wno-error=implicit-function-declaration -fpermissive" ./install.sh "$@"
+  elif command -v pkexec >/dev/null 2>&1; then
+    # Graphical prompt fallback
+    pkexec sh -c 'CFLAGS="-O2 -Wno-error=int-conversion -Wno-error=implicit-function-declaration -fpermissive" ./install.sh "$@"' _ "$@"
+  else
+    echo -e "${RED}ERROR: Neither sudo nor pkexec available.${RES}"
+    exit 1
+  fi
+
+  if [ $? -ne 0 ]; then
     echo -e "${RED}ERROR: Build failed.${RES}"
     rm -rf "${AGH_ROOT}"
     exit 1
   fi
+
   echo -e "${GRE}Build finished successfully!${RES}"
 else
   echo -e "${RED}ERROR: Directory ${AGH_ROOT}/aghues not found after extraction.${RES}"
   exit 1
 fi
+
